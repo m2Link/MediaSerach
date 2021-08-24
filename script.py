8eca8e0d3aed2c9e8d7d5964f5af5b91a9a297b1
Albin-Binu <88622732+Albin-Binu@users.noreply.github.com>
2021-08-24 18:37:39 +0530

    Update script.py

--- script.py
+++ script.py
@@ -9,3 +9,9 @@ Usual commands:
 /start - check whether im online
 /help - get this help message
 """
+
+    START_TEXT = """
+**Hi, I'm Media Search bot**
+
+Here you can search files in inline mode. Just press follwing buttons and start searching.
+"""

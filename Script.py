cdc3c432d36d42a47a471baecb9ab209376c33be
Albin-Binu <88622732+Albin-Binu@users.noreply.github.com>
2021-08-24 18:29:01 +0530

    Create Script.py

---
+++ Script.py
@@ -0,0 +1,11 @@
+class Script(object):
+
+
+    HELP_MSG = """
+What is a filter bot?
+A bot were group admins can set replies for a particular keyword and the bot will automatically send preset replies whenever that keyword enountered in the chat.
+
+Usual commands:
+/start - check whether im online
+/help - get this help message
+"""

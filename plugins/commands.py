f7cc88b917eedcfbbcbad58a31ec3189ab0bb3e2
Albin-Binu <88622732+Albin-Binu@users.noreply.github.com>
2021-08-24 18:52:08 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -93,7 +93,7 @@ async def start(bot, cmd):
             )
         )
     else:
-        await cmd.send_message(
+        await bot.send_message(
             text=Script.START_TEXT,
             parse_mode="Markdown",
             disable_web_page_preview=True,

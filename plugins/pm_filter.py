91df4a88a99ea435787d8222ec693fae3e540987
ᴬᴸᴮᴵᴺ ᴮᴵᴺᵁ <88622732+Albin-Binu@users.noreply.github.com>
2021-08-26 16:54:33 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -287,21 +287,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
             disable_web_page_preview=True
         )
         return
-       elif query.data == "about":
-            await query.answer()
-            keyboard = InlineKeyboardMarkup(
-                   [
-               [
-                   InlineKeyboardButton('Update Channel', url='https://t.me/subin_works'),
-                   InlineKeyboardButton('Source Code', url='https://github.com/subinps/Media-Search-bot')
-               ]
-            ]
-        )
-        await query.message.edit_(
-             Script.HELP_MSG,
-             reply_markup=keyboard,
-             disable_web_page_preview=True
-       )
 
         elif query.data.startswith("albin_binu"):
             ident, file_id = query.data.split("#")

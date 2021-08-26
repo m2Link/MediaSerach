3a2520d421fc74e5f5a1c7738d36b9b43b2169dc
ᴬᴸᴮᴵᴺ ᴮᴵᴺᵁ <88622732+Albin-Binu@users.noreply.github.com>
2021-08-26 17:13:34 +0530

    🙄

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -98,7 +98,20 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 ]
             await query.message.edit(text="<b>Developer : <a href='https://t.me/subinps_bot'>SUBIN</a>\nLanguage : <code>Python3</code>\nLibrary : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a>\nSource Code : <a href='https://github.com/subinps/Media-Search-bot'>Click here</a>\nUpdate Channel : <a href='https://t.me/subin_works'>XTZ Bots</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
 
-
+     
+        elif quey.data == "howto":
+            buttons = [
+                [
+                    InlineKeyboardButton('Update Channel', url='https://t.me/subin_works'),
+                    InlineKeyboardButton('Source Code', url='https://github.com/subinps/Media-Search-bot')
+                ]
+                ]
+            await query.message.edit(
+            Script.HELP_MSG,
+            reply_markup=keyboard,
+            disable_web_page_preview=True
+        )
+        return
 
         elif query.data.startswith("subinps"):
             ident, file_id = query.data.split("#")

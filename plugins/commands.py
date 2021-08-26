8227020c20376a6c5361276ff7d70f4283ef0273
Albin-Binu <88622732+Albin-Binu@users.noreply.github.com>
2021-08-26 09:10:27 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -96,7 +96,7 @@ async def start(bot, cmd):
         )
     else:
         await cmd.reply_photo(
-            chat_id=update.chat.id,
+            chat_id=cmd.from_user.id,
             photo="https://telegra.ph/file/f8ec23f51459806ff12ba.jpg",
             text=Script.START_TEXT,
             parse_mode="Markdown",

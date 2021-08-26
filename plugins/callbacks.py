c1c9f15827f096ff41030643eafbf6dd8c3f1766
ᴬᴸᴮᴵᴺ ᴮᴵᴺᵁ <88622732+Albin-Binu@users.noreply.github.com>
2021-08-26 17:44:33 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -98,6 +98,18 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 ]
             await query.message.edit(Script.ABOUT_TEXT, reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
 
+        elif query.data == "helpz":
+            buttons = [
+                [
+                    InlineKeyboardButton('Basic help', callback_data="howto"),
+                    InlineKeyboardButton('About me', callback_data="about")
+                ],
+                [
+                    InlineKeyboardButton('go inline', switch_inline_query_current_chat='')
+                ],
+                ]
+            await query.message.edit(Script.HELPZ_TEXT, reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+
      
         elif query.data == "howto":
             buttons = [

1646a45e157e3e8f5d8ece5644378484c03ce619
ᴬᴸᴮᴵᴺ ᴮᴵᴺᵁ <88622732+Albin-Binu@users.noreply.github.com>
2021-08-27 23:51:02 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -126,7 +126,7 @@ async def start(bot, cmd):
             )
         )
     else:
-        await cmd.reply_sticker(random.choice(STICKERS))
+        await cmd.send_sticker(random.choice(STICKERS))
             chat_id=cmd.from_user.id
             reply_markup=InlineKeyboardMarkup(
                 [

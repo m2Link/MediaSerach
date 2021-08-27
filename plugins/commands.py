e8cc6eb0bfa09f73bf6522db8cc0dddac113dcef
ᴬᴸᴮᴵᴺ ᴮᴵᴺᵁ <88622732+Albin-Binu@users.noreply.github.com>
2021-08-27 19:46:19 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -82,9 +82,9 @@ async def start(bot, cmd):
             await cmd.reply_text(f"Something went wrong!\n\n**Error:** `{err}`")
     elif len(cmd.command) > 1 and cmd.command[1] == 'subscribe':
         invite_link = await bot.create_chat_invite_link(int(AUTH_CHANNEL))
-        await bot.send_photo(
+        await bot.send_sticker(
             chat_id=cmd.from_user.id,
-            photo="https://telegra.ph/file/d433fcff094ed716d0c7b.jpg",
+            sticker="CAACAgIAAx0CSMbgUAABDlW1YSjtrvv3AbIfiwMboryAbsLFtf8AAnAAA6_GURr1rq3xgoZ54B4E",
             caption="**Please Join My Updates Channel to use this Bot!**",
             reply_markup=InlineKeyboardMarkup(
                 [
@@ -95,8 +95,8 @@ async def start(bot, cmd):
             )
         )
     else:
-        await cmd.reply_photo(
-            photo="https://telegra.ph/file/3bb651eee9e041e7eb8a8.jpg",
+        await cmd.reply_sticker(
+            sticker="CAACAgIAAx0CSMbgUAABDlW1YSjtrvv3AbIfiwMboryAbsLFtf8AAnAAA6_GURr1rq3xgoZ54B4E",
             caption=Script.START_TEXT,
             parse_mode="Markdown",
             reply_markup=InlineKeyboardMarkup(

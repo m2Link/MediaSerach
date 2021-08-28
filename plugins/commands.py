c962abaf963948b26c909a10650254cb82d0069e
ᴬᴸᴮᴵᴺ ᴮᴵᴺᵁ <88622732+Albin-Binu@users.noreply.github.com>
2021-08-28 13:19:12 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -2,13 +2,22 @@
 import os
 import logging
 from pyrogram import Client, filters
-
+import random
 from script import Script
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from info import CHANNELS, ADMINS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION
 from utils import Media, get_file_details
 from pyrogram.errors import UserNotParticipant
 logger = logging.getLogger(__name__)
+
+            "https://telegra.ph/file/082945d6b02f4f61922c4.jpg",
+            "https://telegra.ph/file/3bf7192f061335da05fe5.jpg",
+            "https://telegra.ph/file/a99244baddea1a42ae0d3.jpg",
+            "https://telegra.ph/file/06ee9d64c955652c73c07.jpg",
+            "https://telegra.ph/file/99e183d3cc4d1d8642067.jpg",
+            "https://telegra.ph/file/0540169f6f79228280088.jpg",
+            "https://telegra.ph/file/fb97821d617db86a8f158.jpg",
+            "https://telegra.ph/file/0b0e1f9827f7e6f156d73.jpg",
 
 @Client.on_message(filters.command("start"))
 async def start(bot, cmd):
